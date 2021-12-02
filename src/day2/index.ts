9a7a88d1fcbcb0d64f34174778b1b3170a56af1d
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2021-12-02 12:00:57 +0100

    feat: day 2 part 1

---
+++ src/day2/index.ts
@@ -0,0 +1,64 @@
+import { input as rawInput } from "./input";
+
+type Command = {
+  kind: "forward" | "up" | "down";
+  value: number;
+};
+
+async function part1() {
+  const input = rawInput.split("\n").map(parseCommand);
+
+  let horizontalPosition = 0;
+  let depth = 0;
+
+  for (const command of input) {
+    switch (command.kind) {
+      case "forward": {
+        horizontalPosition += command.value;
+        break;
+      }
+      case "down": {
+        depth += command.value;
+        break;
+      }
+      case "up": {
+        depth -= command.value;
+        break;
+      }
+    }
+  }
+
+  return horizontalPosition * depth;
+}
+
+async function part2() {}
+
+function parseCommand(line: string): Command {
+  const [kind, rawValue] = line.split(" ");
+
+  if (!isCommandKind(kind)) {
+    throw new Error(`Unknown command: ${kind}`);
+  }
+
+  const value = Number(rawValue);
+
+  if (isNaN(value)) {
+    throw new Error(`Invalid value: ${rawValue}`);
+  }
+
+  return {
+    kind: kind as Command["kind"],
+    value,
+  };
+}
+
+function isCommandKind(kind: string): kind is Command["kind"] {
+  return ["forward", "up", "down"].includes(kind);
+}
+
+async function main() {
+  console.log(`⭐️ Part 1: ${await part1()}`);
+  console.log(`⭐️ Part 2: ${await part2()}`);
+}
+
+main();
