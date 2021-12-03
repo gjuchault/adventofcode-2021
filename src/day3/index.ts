a52c99dc9e8441f6d75597481c8f1497fb2562b6
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2021-12-03 23:44:35 +0100

    feat: day 3 part 2

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -6,10 +6,10 @@ async function part1() {
     .split("\n")
     .map((line) => line.split("").map((n) => Number(n)));
 
-  const transposed = transpose(diagnostic);
+  const transposedDiagnostic = transpose(diagnostic);
 
-  const gammaArray = transposed.map((row) =>
-    row.filter((item) => item === 1).length > row.length / 2 ? 1 : 0
+  const gammaArray = transposedDiagnostic.map((row) =>
+    row.filter((item) => item === 1).length >= row.length / 2 ? 1 : 0
   );
   const gamma = parseInt(gammaArray.join(""), 2);
 
@@ -21,7 +21,62 @@ async function part1() {
   return gamma * epsilon;
 }
 
-async function part2() {}
+async function part2() {
+  const diagnostic = rawInput
+    .split("\n")
+    .map((line) => line.split("").map((n) => Number(n)));
+
+  const oxygenGeneratorRating = filterDiagnostic(diagnostic, (row) => {
+    const mostCommonBit =
+      row.filter((item) => item === 1).length >= row.length / 2 ? 1 : 0;
+
+    return (item) => item === mostCommonBit;
+  });
+
+  const co2ScrubberRating = filterDiagnostic(diagnostic, (row) => {
+    const leastCommonBit =
+      row.filter((item) => item === 1).length >= row.length / 2 ? 0 : 1;
+
+    return (item) => item === leastCommonBit;
+  });
+
+  return oxygenGeneratorRating * co2ScrubberRating;
+}
+
+function filterDiagnostic(
+  diagnostic: number[][],
+  createTakeBit: (row: number[]) => (item: number) => boolean
+) {
+  let transposedDiagnostic = transpose(diagnostic);
+
+  let column = 0;
+  while (true) {
+    const row = transposedDiagnostic[column];
+    const takeBit = createTakeBit(row);
+
+    let indexesToRemove = [];
+
+    for (let i = 0; i < row.length; i++) {
+      if (!takeBit(row[i])) {
+        indexesToRemove.push(i);
+      }
+    }
+
+    const nextTransposedDiagnostic = transposedDiagnostic.map((row) =>
+      row.filter((_, index) => !indexesToRemove.includes(index))
+    );
+
+    transposedDiagnostic = nextTransposedDiagnostic;
+
+    if (transposedDiagnostic[0].length === 1) {
+      break;
+    }
+
+    column += 1;
+  }
+
+  return parseInt(transposedDiagnostic.map((item) => item[0]).join(""), 2);
+}
 
 async function main() {
   console.log(`⭐️ Part 1: ${await part1()}`);
