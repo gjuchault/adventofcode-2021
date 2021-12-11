713285b2349fac766d210713524b4e8a971aa711
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2021-12-11 01:44:39 +0100

    feat: day 6 part 1

---
+++ src/day6/index.ts
@@ -0,0 +1,50 @@
+import { input as rawInput } from "./input";
+
+async function part1() {
+  const fishes = rawInput.split(",").map(Number);
+
+  let fishByReset = new Map([
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+    [5, 0],
+    [6, 0],
+    [7, 0],
+    [8, 0],
+  ]);
+
+  for (const fish of fishes) {
+    fishByReset.set(fish, (fishByReset.get(fish) ?? 0) + 1);
+  }
+
+  for (let i = 0; i < 80; i++) {
+    fishByReset = new Map([
+      [0, fishByReset.get(1) ?? 0],
+      [1, fishByReset.get(2) ?? 0],
+      [2, fishByReset.get(3) ?? 0],
+      [3, fishByReset.get(4) ?? 0],
+      [4, fishByReset.get(5) ?? 0],
+      [5, fishByReset.get(6) ?? 0],
+      [6, (fishByReset.get(7) ?? 0) + (fishByReset.get(0) ?? 0)],
+      [7, fishByReset.get(8) ?? 0],
+      [8, fishByReset.get(0) ?? 0],
+    ]);
+  }
+
+  return Array.from(fishByReset.values()).reduce((a, b) => a + b, 0);
+}
+
+async function part2() {
+  const input = rawInput.split(",").map(Number);
+
+  return input.length;
+}
+
+async function main() {
+  console.log(`⭐️ Part 1: ${await part1()}`);
+  console.log(`⭐️ Part 2: ${await part2()}`);
+}
+
+main();
